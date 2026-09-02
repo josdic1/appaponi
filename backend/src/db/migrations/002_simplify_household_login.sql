9d15afd86cbb620d2509bc0f66eed0cc7d553348
josdic1 <joshuadicker@gmail.com>
2026-09-02 10:31:32 -0400

    Freeze canonical Appoponi schema

--- backend/src/db/migrations/002_simplify_household_login.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE account_households;
-
-ALTER TABLE households
-ADD COLUMN account_id BIGINT NOT NULL
-  REFERENCES accounts(id)
-  ON DELETE CASCADE;
-
-CREATE UNIQUE INDEX households_one_account
-ON households (account_id);
